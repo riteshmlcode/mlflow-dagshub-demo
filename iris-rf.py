bb5eb043bf11e8d9e7b38781b9243a2302a1ee99
xcode <xcode@xcode.com>
2025-01-18 11:39:49 +0530

    RF Experiment

--- iris-rf.py
+++ iris-rf.py
@@ -1,7 +1,7 @@
 import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_iris
-from sklearn.ensemble import RandormForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -30,7 +30,7 @@ mlflow.set_experiment('iris-dt')
 
 with mlflow.start_run():
     # Create a random forest classifier
-    rf = RandormForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
     rf.fit(X_train, y_train)
 
     # Make predictions
